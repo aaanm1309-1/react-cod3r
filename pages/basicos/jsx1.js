08d3b5ca0b7915f30132518da21244b7d6906470
aaanm13 <aaanm1309.1@gmail.com>
2021-07-13 12:36:36 -0300

    revisao jsx2

---
+++ pages/basicos/jsx1.js
@@ -0,0 +1,9 @@
+//jsx
+
+export default function jsx1Diferente() {
+  return (
+    <div>
+      <h1>jsx #1</h1>
+    </div>
+  )
+}
---
+++ pages/teste/segunda_pagina.tsx
@@ -0,0 +1,3 @@
+export default function segundaPagina() {
+  return "Index teste - segunda Pagina"
+}
