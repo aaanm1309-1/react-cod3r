5b64a155589eeba5b96caab2f6d5b96455685677
aaanm13 <aaanm1309.1@gmail.com>
2021-07-13 14:15:36 -0300

    versao listas

---
+++ pages/basicos/lista.jsx
@@ -0,0 +1,34 @@
+export default function lista() {
+  const lista1 = [
+    "Lista 1",
+    "Lista 2",
+    "Lista 3",
+    "Lista 4",
+    "Lista 5",
+    "Lista 6",
+    "Lista 7",
+    "Lista 8",
+    "Lista 9",
+
+  ];
+
+  return (
+    <div>
+      <h1>Teste</h1>  
+      <ul>
+        {lista1.map(item => <li key={item}>{item}</li>)}
+      </ul>
+      {gerarLista()}
+    </div>
+  )
+}
+
+
+function gerarLista(final = 10) {
+  const lista = [];
+  for(let i = 1; i <= final; i++) {
+    lista.push(<span key={i}>{i},</span>)
+  }
+  return lista
+  
+}
